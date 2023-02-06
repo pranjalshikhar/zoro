717e735043df9c1050316df69bb69a1808cbf140
Pranjal Shikhar Sinha <shikhar.pranjal3@gmail.com>
2023-02-06 16:20:19 +0530

    (ft): 🤯 added header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,39 @@
-import React from "react";
+import { AppBar, Toolbar, Typography, IconButton } from "@mui/material";
+import { Menu } from "@mui/icons-material";
+import { styled } from "@mui/material/styles";
 
-const Header = () => {
-  return <div>Header</div>;
+const Head = styled(AppBar)`
+  z-index: 1201;
+  background: #fff;
+  height: 70px;
+  box-shadow: inset 0 -1px 0 0 #dadce0;
+`;
+
+const Heading = styled(Typography)`
+  color: #5f6368;
+  font-size: 24px;
+  margin-left: 25px;
+`;
+
+const Header = ({ open, handleDrawer }) => {
+  const logo =
+    "https://seeklogo.com/images/G/google-keep-logo-0BC92EBBBD-seeklogo.com.png";
+
+  return (
+    <Head open={open}>
+      <Toolbar>
+        <IconButton
+          onClick={() => handleDrawer()}
+          sx={{ marginRight: "20px" }}
+          edge="start"
+        >
+          <Menu />
+        </IconButton>
+        <img src={logo} alt="logo" style={{ width: 30 }} />
+        <Heading>Zorro</Heading>
+      </Toolbar>
+    </Head>
+  );
 };
 
 export default Header;
