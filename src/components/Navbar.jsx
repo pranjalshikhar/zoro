5bad383acf4ba8ee80d326e1cb6255307c3ccc60
Pranjal Shikhar Sinha <shikhar.pranjal3@gmail.com>
2023-02-06 16:09:19 +0530

    (ft): 🧭 created navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,38 @@
-import React from "react";
+import { List, ListItem, ListItemIcon, ListItemText } from "@mui/material";
+import {
+  LightbulbOutlined as Lightbulb,
+  ArchiveOutlined as Archive,
+  DeleteOutlined as Delete,
+} from "@mui/icons-material";
+import { Link } from "react-router-dom";
+
+const list = [
+  { id: 1, name: "Notes", icon: <Lightbulb />, route: "/" },
+  { id: 2, name: "Archives", icon: <Archive />, route: "/archives" },
+  { id: 3, name: "Trash", icon: <Delete />, route: "/delete" },
+];
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <List>
+      {list.map((item) => (
+        <ListItem button key={item.id}>
+          <Link
+            to={`#{item.route}`}
+            style={{
+              textDecoration: "none",
+              display: "flex",
+              color: "inherit",
+            }}
+          >
+            <ListItemIcon style={{ alignItems: "center" }}>
+              {item.icon}
+            </ListItemIcon>
+          </Link>
+        </ListItem>
+      ))}
+    </List>
+  );
 };
 
 export default Navbar;
