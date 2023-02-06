75444eb801532ff0491fa3cdccee44ed6ed27f86
Pranjal Shikhar Sinha <shikhar.pranjal3@gmail.com>
2023-02-06 15:52:58 +0530

    (ft): 🍥 added swipe drawer

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,11 +3,13 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Archives from "./archive/Archives";
 import DeleteNotes from "./delete/DeleteNotes";
 import Notes from "./notes/Notes";
+import SwipeDrawer from "./SwipeDrawer";
 
 const Home = () => {
   return (
-    <Box>
+    <Box style={{ display: "flex", width: "100%" }}>
       <Router>
+        <SwipeDrawer />
         <Routes>
           <Route path="/" element={<Notes />} />
           <Route path="/archive" element={<Archives />} />
