c8df062d906853cf7a58e7f39901d0313a68715b
Pranjal Shikhar Sinha <shikhar.pranjal3@gmail.com>
2023-02-06 17:19:42 +0530

    (ft): 📝 note component

--- src/components/notes/Note.jsx
+++ src/components/notes/Note.jsx
@@ -1,7 +1,54 @@
-import React from "react";
+import { useContext } from "react";
+import { Card, CardContent, CardActions, Typography } from "@mui/material";
+import { styled } from "@mui/material/styles";
+import {
+  ArchiveOutlined as Archive,
+  DeleteOutlined as Delete,
+} from "@mui/icons-material";
+import { DataContext } from "../../context/DataProvider";
 
-const Note = () => {
-  return <div>Note</div>;
+const StyledCard = styled(Card)`
+  border: 1px solid #e0e0e0;
+  border-radius: 8px;
+  width: 240px;
+  margin: 8px;
+  box-shadow: none;
+`;
+
+const Note = ({ note }) => {
+  const { notes, setNotes, setArchiveNotes, setDeleteNotes } =
+    useContext(DataContext);
+  const archiveNote = (note) => {
+    const updatedNotes = notes.filter((data) => data.id !== note.id);
+    setNotes(updatedNotes);
+    setDeleteNotes((prev) => [note, ...prev]);
+  };
+  const deleteNote = (note) => {
+    const updatedNotes = notes.filter((data) => data.id !== note.id);
+    setNotes(updatedNotes);
+    setDeleteNotes((prev) => [note, ...prev]);
+  };
+
+  return (
+    <StyledCard>
+      <CardContent>
+        <Typography>{note.heading}</Typography>
+        <Typography>{note.text}</Typography>
+      </CardContent>
+      <CardActions>
+        <Archive
+          fontSize="small"
+          style={{ marginLeft: "auto" }}
+          onClick={() => archiveNote(note)}
+        />
+        <Delete
+          fontSize="small"
+          style={{ marginLeft: "auto" }}
+          onClick={() => deleteNote(note)}
+        />
+      </CardActions>
+    </StyledCard>
+  );
 };
 
 export default Note;
