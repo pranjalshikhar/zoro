be979d95b0322b029ffb4fd9a5cdc42d9bc68196
Pranjal Shikhar Sinha <shikhar.pranjal3@gmail.com>
2023-02-06 17:52:03 +0530

    (ft): 🗑️ delete

--- src/components/delete/DeleteNote.jsx
+++ src/components/delete/DeleteNote.jsx
@@ -1,7 +1,57 @@
-import React from "react";
+import { useContext } from "react";
+import { Card, CardContent, CardActions, Typography } from "@mui/material";
+import { styled } from "@mui/material/styles";
+import {
+  RestoreFromTrashOutlined as Restore,
+  DeleteForeverOutlined as Delete,
+} from "@mui/icons-material";
+import { DataContext } from "../../context/DataProvider";
 
-const DeleteNote = () => {
-  return <div>DeleteNote</div>;
+const StyledCard = styled(Card)`
+  border: 1px solid #e0e0e0;
+  border-radius: 8px;
+  width: 240px;
+  margin: 8px;
+  box-shadow: none;
+`;
+
+const DeleteNote = ({ deleteNote }) => {
+  const { deleteNotes, setNotes, setArchiveNotes, setDeleteNotes } =
+    useContext(DataContext);
+  const restoreNote = (deleteNote) => {
+    const updatedNotes = deleteNotes.filter(
+      (data) => data.id !== deleteNote.id
+    );
+    setDeleteNotes(updatedNotes);
+    setNotes((prev) => [deleteNote, ...prev]);
+  };
+  const removeNote = (deleteNote) => {
+    const updatedNotes = deleteNotes.filter(
+      (data) => data.id !== deleteNote.id
+    );
+    setDeleteNotes(updatedNotes);
+  };
+
+  return (
+    <StyledCard>
+      <CardContent>
+        <Typography>{deleteNote.heading}</Typography>
+        <Typography>{deleteNote.text}</Typography>
+      </CardContent>
+      <CardContent>
+        <Delete
+          fontSize="small"
+          style={{ marginLeft: "auto" }}
+          onClick={() => removeNote(deleteNote)}
+        />
+        <Restore
+          fontSize="small"
+          style={{ marginLeft: "auto" }}
+          onClick={() => restoreNote(deleteNote)}
+        />
+      </CardContent>
+    </StyledCard>
+  );
 };
 
 export default DeleteNote;
--- src/components/delete/DeleteNotes.jsx
+++ src/components/delete/DeleteNotes.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import { useContext } from "react";
+import { Box, Grid } from "@mui/material";
+import { styled } from "@mui/material/styles";
+import { DataContext } from "../../context/DataProvider";
+import DeleteNote from "./DeleteNote";
+
+const DrawerHeader = styled("div")(({ theme }) => ({
+  ...theme.mixins.toolbar,
+}));
 
 const DeleteNotes = () => {
-  return <div>DeleteNotes</div>;
+  const { deleteNotes } = useContext(DataContext);
+
+  return (
+    <Box sx={{ display: "flex", widht: "100%" }}>
+      <Box sx={{ p: 3, width: "100%" }}>
+        <DrawerHeader />
+        <Grid container>
+          {deleteNotes.map((deleteNote) => (
+            <Grid item>
+              <DeleteNote deleteNote={deleteNote} />
+            </Grid>
+          ))}
+        </Grid>
+      </Box>
+    </Box>
+  );
 };
 
 export default DeleteNotes;
